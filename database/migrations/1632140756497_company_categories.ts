87fb8bd221323b2fa924ce70fa418da29b0c42d7
Yasser Mussa <yasser.mussa@gmail.com>
2021-10-30 17:06:14 -0300

    .

--- database/migrations/1632140756497_company_categories.ts
+++ database/migrations/1632140756497_company_categories.ts
@@ -11,8 +11,8 @@ export default class CompanyCategories extends BaseSchema {
 			/**
 			 * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
 			 */
-			table.timestamp('created_at', { useTz: true })
-			table.timestamp('updated_at', { useTz: true })
+			table.timestamp('created_at')
+			table.timestamp('updated_at')
 		})
 	}
 
